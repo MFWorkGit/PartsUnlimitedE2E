f3c19e43fb2371ac3c5bf424edeab1291297f089
Matthew Filiatreault <MFILIATR@cmhc-schl.gc.ca>
2020-07-15 16:21:23 -0400

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //new line
+        //Second change
         [Key]
         public int CartItemId { get; set; }
 
